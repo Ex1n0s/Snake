a7082a0efc53cedae08db51d19fee5c348a75fde
Ex1n0s <pisacane.simone19@gmail.com>
2025-12-05 17:19:04 +0100

    Added collisions

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,33 +6,35 @@ import java.util.List;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
-    private int APPLE_SIZE = 20,SEGMENT_SIZE = 10;
+    private int APPLE_SIZE = 20;
+    private int SEGMENT_SIZE = 10;
+    private int SCREEN_WIDTH = 640;
+    private int SCREEN_HEIGHT = 640;
     private Timer gameTimer;
     private int directionX = 0, directionY = 1;
-    private int velocity = 5;
-    private List<Point> snake;
-    private List<Point> storicoPosizioni;
-    private List<Point> apples;
+    private int velocity = 10;
+    private List<Rectangle> snake;
+    private List<Rectangle> apples;
+    private int score = 0;
 
     public GamePanel(){
         setBackground(Color.black);
-        setPreferredSize(new Dimension(640,640));
+        setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
         setOpaque(true);
 
         snake = new ArrayList<>();
-        storicoPosizioni = new ArrayList<>();
         apples = new ArrayList<>();
 
         int x = 100;
-        for (int i = 0; i < 7; i++){
-            snake.add(new Point(x - 20 * i,100));
+        for (int i = 0; i < 4; i++){
+            snake.add(new Rectangle(x - SEGMENT_SIZE * i,100,SEGMENT_SIZE,SEGMENT_SIZE));
         }
         spawnApple(3);
 
         setFocusable(true);
         addKeyListener(new InputHandler());
 
-        gameTimer = new Timer(16,this);
+        gameTimer = new Timer(30,this);
         gameTimer.start();
     }
 
@@ -49,26 +51,14 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     private void moveSnake(){
-        snake.get(0).x += velocity * directionX;
-        snake.get(0).y += velocity * directionY;
-        storicoPosizioni.add(0,new Point(snake.get(0).x,snake.get(0).y));
-
-        for(int i = 1; i < snake.size(); i++){
-            int index = i * 3;
-            if(index < storicoPosizioni.size()){
-                Point puntoStorico = storicoPosizioni.get(index);
-                snake.get(i).x = puntoStorico.x;
-                snake.get(i).y = puntoStorico.y;
-            } else {
-                Point vecchioPunto = storicoPosizioni.get(storicoPosizioni.size() - 1);
-                snake.get(i).x = vecchioPunto.x;
-                snake.get(i).y = vecchioPunto.y;
-            }
+        for(int i = snake.size() - 1; i > 0; i--){
+            Rectangle current = snake.get(i);
+            Rectangle next = snake.get(i - 1);
+            current.setLocation(next.x,next.y);
         }
-        int maxStoria = snake.size() * 3;
-        if(storicoPosizioni.size() > maxStoria){
-            storicoPosizioni.remove(storicoPosizioni.size() - 1);
-        }
+        Rectangle head = snake.get(0);
+        head.x += velocity * directionX;
+        head.y += velocity * directionY;
     }
 
     @Override
@@ -76,18 +66,18 @@ public class GamePanel extends JPanel implements ActionListener {
         super.paintComponent(g);
         //Draw dei segmenti del serpente
         for (int i = 0; i < snake.size(); i++) {
-            Point point = snake.get(i);
+            Rectangle segment = snake.get(i);
             if(i == 0){
                 g.setColor(Color.RED);
             } else {
                 g.setColor(Color.GREEN);
             }
-            g.fillRect(point.x,point.y,SEGMENT_SIZE,SEGMENT_SIZE);
-            //g.fillRoundRect(point.x, point.y,20,20,20,20);
+            g.fillRect(segment.x,segment.y,SEGMENT_SIZE,SEGMENT_SIZE);
         }
+        g.setColor(Color.YELLOW);
         for (int i = 0; i < apples.size(); i++) {
-            Point point = apples.get(i);
-            g.fillRoundRect(point.x, point.y,APPLE_SIZE,APPLE_SIZE,20,20);
+            Rectangle apple = apples.get(i);
+            g.fillRoundRect(apple.x, apple.y,APPLE_SIZE,APPLE_SIZE,20,20);
         }
     }
 
@@ -96,51 +86,59 @@ public class GamePanel extends JPanel implements ActionListener {
         for (int i = 0; i < n; i++) {
             int x = random.nextInt(640);
             int y = random.nextInt(640);
-            apples.add(new Point(x,y));
+            apples.add(new Rectangle(x,y,APPLE_SIZE,APPLE_SIZE));
         }
     }
 
     public void checkCollisions(){
-        for (int i = 0; i < apples.size(); i++) {
-            Point headPosition = snake.getFirst();
-            Point applePosition = apples.get(i);
-            if(headPosition.equals(applePosition)){
-                apples.remove(i);
-                i--;
+        Rectangle head = snake.getFirst();
+        for (int i = 1; i < snake.size(); i++){
+            Rectangle segment = snake.get(i);
+            if(head.intersects(segment)){
+                gameOver();
+                return;
             }
         }
+
+        for (int i = 0; i < apples.size(); i++) {
+            Rectangle apple = apples.get(i);
+            if(head.intersects(apple)){
+                score++;
+                apples.remove(i);
+                i--;
+                snake.add(new Rectangle(-100,-100,SEGMENT_SIZE,SEGMENT_SIZE));
+                spawnApple(1);
+            }
+        }
+
+        if(head.x < 0 || head.x >= SCREEN_WIDTH || head.y < 0 || head.y >= SCREEN_HEIGHT){
+            gameOver();
+        }
+
+    }
+
+    private void gameOver(){
+        gameTimer.stop();
+        JOptionPane.showMessageDialog(this,"Game over\npunteggio: " + score);
     }
 
     private class InputHandler extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
-            switch (key){
-                case KeyEvent.VK_W:
-                    if(directionY != 1){
-                        directionX = 0;
-                        directionY = -1;
-                    }
-                    break;
-                case KeyEvent.VK_S:
-                    if(directionY != -1){
-                        directionX = 0;
-                        directionY = 1;
-                    }
-                    break;
-                case KeyEvent.VK_D:
-                    if(directionX != -1){
-                        directionY = 0;
-                        directionX = 1;
-                    }
-                    break;
-                case KeyEvent.VK_A:
-                    if (directionX != 1) {
-                        directionY = 0;
-                        directionX = -1;
-                    }
-                default:
-                    break;
+
+            if(key == KeyEvent.VK_W && directionY != 1){
+                directionX = 0;
+                directionY = -1;
+            } else if (key == KeyEvent.VK_S && directionY != -1) {
+                directionX = 0;
+                directionY = 1;
+            } else if (key == KeyEvent.VK_D && directionX != -1) {
+                directionY = 0;
+                directionX = 1;
+            } else if (key == KeyEvent.VK_A && directionX != 1) {
+                directionY = 0;
+                directionX = -1;
             }
         }
     }
